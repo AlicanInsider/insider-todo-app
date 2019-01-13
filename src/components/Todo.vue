7712cc91c667d442aec7a03a2cce0dbab456247d
AlicanInsider <alican.camlibel@useinsider.com>
2019-01-14 00:25:15 +0300

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="task">
+  <input :value="this.title" :class="[{ notdone : this.isTodoNotDone} , {done:this.isTodoDone}]">
+  <div class="button-container">
+    <button v-on:click="removeTodo">Delete</button>
+    <button v-on:click="doneTodo">Done</button>
+    <button v-on:click="notDoneTodo">Not Done</button>
+  </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: ['title', 'taskIndex'],
+  methods: {
+        removeTodo: function () {
+            this.$emit('removeEvent', this.taskIndex);
+        },
+        doneTodo: function(){
+          this.isTodoDone = !this.isTodoDone ;
+          this.isTodoNotDone= false;
+        },
+        notDoneTodo: function(){
+          this.isTodoNotDone = !this.isTodoNotDone;
+          this.isTodoDone= false;
+        }
+    },
+    data: function() {
+      return {
+        isTodoDone: false,
+        isTodoNotDone: false
+    }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.task{
+  display:flex;
+}
+button{
+  padding:10px;
+}
+.task input{
+  flex:1;
+  padding:10px;
+}
+.notdone {
+  background:red;
+}
+.done{
+  background:green;
+}
+</style>
---
+++ src/components/Todolist.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="task">
+  <div class="task-container">
+    <input placeholder="Task name" type="text" v-model="title" class="task-container-input">
+    <button v-on:click="addTask">Click Me</button>
+       <div v-for="(todo,index) in todos">
+        <Todo v-bind:title="todo.title" v-on:removeEvent="removeTodo" v-bind:taskIndex="index"></Todo>
+       </div>
+  </div>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo.vue'
+
+export default {
+  name: 'Todolist',
+  components: {
+    Todo
+  },
+  data: function() {
+      return {
+          title:'',
+          todos: [{
+            title: 'Todo A'
+        }, {
+            title: 'Todo B'
+        }, {
+            title: 'Todo C'
+        }, {
+            title: 'Todo D'
+        }],
+      }
+  },
+  methods: {
+        addTask:function(){
+            if(this.title.length > 0){
+                this.todos.push({title:this.title});
+            }
+            
+            this.title = '';
+        },
+        removeTodo: function(index) {
+            this.todos.splice(index, 1);
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.task-container{
+    width:500px;
+    height:500px;
+    position:absolute;
+    top:50%;
+    left:50%;
+    transform:translate(-50%,-50%);
+}
+.task-container-input{
+    width:99.2%;
+}
+button{
+    width:100%;
+}
+</style>
